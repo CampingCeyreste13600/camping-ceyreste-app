053d8a77dfa3fffad0f34a9916fc66be19d989b6
CampingCeyreste13600 <campingceyreste@gmail.com>
2026-08-24 17:29:05 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Camping de Ceyreste - cache réseau robuste */
-const CACHE_VERSION = "ceyreste-v2026-08-19-10";
+const CACHE_VERSION = "ceyreste-v2026-08-24-12";
 const CORE = [
   "./",
   "./index.html",
